c9ccf0c504ee14dde110a55dbfb4c4c30679eda6
stuckinadessert <stuckinadessert@gmail.com>
2017-04-21 00:43:04 -0700

    Added divide and conquer function (WIP)
    Set up the function shell for the divide and conquer call with test
    inputs. Currently does nothing.
    CHANGES:
    Altered filepath to look in current directory for input file.
    Added a test call to the input stream. Currently does not modify.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@ using namespace std;
 //Max array size is 26 based on txt file
 const int MAX_ARRAY_SIZE = 100;
 
+//Function for Divide and Conquer Call WIP.
+int dnc(int array[100], int start, int end, int *max, int *maxStart, int *maxEnd){
+//This section to test inputs.
+cout << "\nsize of array is " << end << "\n";
+cout << "passed in value of max is " << *max << "\n";
+cout << "position of max is " << max << "\n";
+cout << "the first element in the array is " << array[start] << "\n\n";
+cout << "\n" << *maxStart << " " << *maxEnd << "\n";
+
+}
+
 int main()
 {
     //line of text that holds each array
@@ -24,12 +35,12 @@ int main()
     //subarray start and end positions
     int MaxSumStart = 0;
     int MaxSumEnd = 0;
-    
+ 
     //initialize array to 0 values
     int a[MAX_ARRAY_SIZE] = {0};
     
     //open MSS_Problems.txt file (will need to change file path obvioiusly)
-    ifstream myfile( "/Users/westonse/Library/Autosave Information/Project1/Project1/MSS_Problems.txt" );
+    ifstream myfile( "./MSS_Problems.txt" );
     
     //if file is valid open it
     if(myfile)
@@ -48,17 +59,18 @@ int main()
                 //once size of array is found parse the current array and determine max subarray
                 if(!stream)
                 {
+		//METHOD 3: Divide and Conquer
+			int maxSum=0;
+			dnc(a,0,arraySize-1,&maxSum, &MaxSumStart, &MaxSumEnd);
+			maxSum=0;
                     //METHOD 1: Brute force//
                     //Currently only determines maximum sum
                     //Need to also determine the sub array
-                    for (int i = 0; i <= arraySize-2; i++)
-                    {
+                    for (int i = 0; i <= arraySize-2; i++){
                         sum = 0;
-                        for (int j = i; j <= arraySize - 1; j++)
-                        {
+                        for (int j = i; j <= arraySize - 1; j++){
                             sum = sum + a[j];
-                            if (sum > ret)
-                            {
+                            if (sum > ret){
                                 MaxSumStart = i;
                                 MaxSumEnd = j;
                                 ret = sum;
@@ -68,7 +80,7 @@ int main()
                     //Currently displaying results to console (for ease)
                     //Need to display results in txt file for final submission
                     cout<<"Maximum subarray sum:"<<ret<<"\n";
-                    cout << "Max Sum Start Index: " << MaxSumStart << "  Max Sum End Index : " << MaxSumEnd << "\n";
+                    cout<< "Max Sum Start Index: " << MaxSumStart << "  Max Sum End Index : " << MaxSumEnd << "\n";
                     std::fill_n(a, arraySize, 0);
                     sum = 0;
                     ret = 0;
@@ -84,5 +96,6 @@ int main()
         }
     }
     myfile.close();
+
     return 0;
 }
